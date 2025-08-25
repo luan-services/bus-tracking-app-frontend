1055edc5f27e692c4624fabeb3d23f109d8f4554
luan-services <luandotservices@gmail.com>
2025-08-24 23:55:10 -0300

    update: changed home page design

--- src/components/public_pages/PublicNavbar.tsx
+++ src/components/public_pages/PublicNavbar.tsx
@@ -1,59 +1,55 @@
 "use client"
 import Link from 'next/link';
 import { useState } from 'react';
+import { Menu, X} from 'lucide-react';
 
 export const PublicNavbar = () => {
     // State to manage whether the mobile menu is open or closed
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <nav className="bg-white shadow-md">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-16">
-                    {/* Brand/Logo */}
-                    <div className="flex-shrink-0">
-                        <Link href="/" className="text-2xl font-bold text-indigo-600">
-                            BusTrack
+        <nav className="flex w-full px-4 md:px-20 bg-white justify-center shadow-md">
+            <div className="flex container items-center justify-between min-h-14"> {/* container p impedir crescimento excessivo */}
+
+                {/* Brand/Logo */}
+                <div className="flex">
+                    <Link href="/" className="text-2xl font-bold text-green-700">
+                        BusTrack
+                    </Link>
+                </div>
+
+                {/* Desktop Menu */}
+                <div className="hidden md:flex">
+                    <div className="ml-10 flex items-baseline space-x-4">
+                        <Link href="/" className="text-gray-600 hover:bg-gray-100 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">
+                            Início
+                        </Link>
+                        <Link href="/#linhas" className="text-gray-600 hover:bg-gray-100 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">
+                            Linhas
+                        </Link>
+                        <Link href="/dashboard" className="text-gray-600 hover:bg-gray-100 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">
+                            Acesso Restrito
                         </Link>
                     </div>
+                </div>
 
-                    {/* Desktop Menu */}
-                    <div className="hidden md:block">
-                        <div className="ml-10 flex items-baseline space-x-4">
-                            <Link href="/" className="text-gray-600 hover:bg-gray-100 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">
-                                Início
-                            </Link>
-                            <Link href="/#linhas" className="text-gray-600 hover:bg-gray-100 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">
-                                Linhas
-                            </Link>
-                            <Link href="/dashboard" className="text-gray-600 hover:bg-gray-100 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">
-                                Acesso Restrito
-                            </Link>
-                        </div>
-                    </div>
-
-                    {/* Hamburger Button (Mobile) 🍔 */}
-                    <div className="-mr-2 flex md:hidden">
-                        <button
-                            onClick={() => setIsOpen(!isOpen)}
-                            type="button"
-                            className="bg-gray-100 inline-flex items-center justify-center p-2 rounded-md text-gray-500 hover:text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-                            aria-controls="mobile-menu"
-                            aria-expanded="false"
-                        >
-                            <span className="sr-only">Abrir menu principal</span>
-                            {/* Icon: Menu (hamburger) when closed, X when open */}
-                            {!isOpen ? (
-                                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                                </svg>
-                            ) : (
-                                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            )}
-                        </button>
-                    </div>
+                {/* Hamburger Button (Mobile) 🍔 */}
+                <div className="-mr-2 flex md:hidden">
+                    <button
+                        onClick={() => setIsOpen(!isOpen)}
+                        type="button"
+                        className="bg-gray-100 inline-flex items-center justify-center p-2 rounded-md text-gray-500 hover:text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+                        aria-controls="mobile-menu"
+                        aria-expanded="false"
+                    >
+                        <span className="sr-only">Abrir menu principal</span>
+                        {/* Icon: Menu (hamburger) when closed, X when open */}
+                        {!isOpen ? (
+                            <Menu strokeWidth={2.5} size={20}></Menu>
+                        ) : (
+                            <X strokeWidth={2.5} size={20}></X>
+                        )}
+                    </button>
                 </div>
             </div>
 
