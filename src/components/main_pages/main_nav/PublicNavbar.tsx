210badf1eca6c0c90cb99290338b40ba51204915
luan-services <luandotservices@gmail.com>
2025-08-25 17:33:09 -0300

    update: navmenu style change

--- src/components/main_pages/main_nav/PublicNavbar.tsx
+++ src/components/main_pages/main_nav/PublicNavbar.tsx
@@ -7,6 +7,14 @@ export const PublicNavbar = () => {
     // State to manage whether the mobile menu is open or closed
     const [isOpen, setIsOpen] = useState(false);
 
+    const toggleMobileMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMobileMenu = () => {
+        setIsOpen(false);
+    };
+
     return (
         // Set position to relative to contain the absolutely positioned menu
         <nav className="relative flex w-full bg-white justify-center shadow-md border-b-1 border-gray-300">
@@ -22,21 +30,29 @@ export const PublicNavbar = () => {
 
                 {/* menu desktop */}
                 <div className="hidden md:flex gap-4">
-                        <Link href="/" className="text-gray-600 px-3 py-2 rounded-md text-sm transition duration-200 hover:bg-green-1 hover:text-green-900 font-medium ">
+                    <Link className="px-3 py-2 rounded-md" href="/">
+                        <span className="text-gray-600 text-sm transition duration-200 hover:bg-green-1 hover:text-green-900 font-medium">
                             Início
-                        </Link>
-                        <Link href="/lines" className="text-gray-600 px-3 py-2 rounded-md text-sm transition duration-200 hover:bg-green-1 hover:text-green-900 font-medium ">
+                        </span>
+                    </Link>
+                    
+                    <Link className="px-3 py-2 rounded-md"  href="/lines">
+                        <span className="text-gray-600  text-sm transition duration-200 hover:bg-green-1 hover:text-green-900 font-medium">
                             Linhas
-                        </Link>
-                        <Link href="/login" className="text-gray-600 px-3 py-2 rounded-md text-sm transition duration-200 hover:bg-green-1 hover:text-green-900 font-medium ">
+                        </span>
+                    </Link>
+                    
+                    <Link className="px-3 py-2 rounded-md"  href="/login">
+                        <span className="text-gray-600 text-sm transition duration-200 hover:bg-green-1 hover:text-green-900 font-medium">
                             Acesso Restrito
-                        </Link>
+                        </span>
+                    </Link>
                 </div>
 
                 {/* botão hamburguer (mobile) */}
                 <div className="flex md:hidden">
-                    <button className="bg-gray-100 inline-flex items-center justify-center p-2 rounded-lg active:scale-95 transition duration-300 text-gray-500 hover:text-gray-700 hover:bg-gray-200 focus:outline-0 border-1 border-gray-200" 
-                        onClick={() => setIsOpen(!isOpen)} type="button">
+                    <button className="bg-gray-200 items-center justify-center p-2 rounded-lg active:scale-80 transition duration-300 text-gray-500 focus:outline-none border-1 border-gray-300" 
+                        onClick={() => toggleMobileMenu()} type="button">
                             <Menu strokeWidth={2.5} size={20}></Menu>
                     </button>
                 </div>
@@ -47,7 +63,7 @@ export const PublicNavbar = () => {
 
             {/* menu backdrop */}
             {isOpen &&
-                <div className="fixed inset-0 bg-black opacity-50 z-[2000] md:hidden" onClick={() => setIsOpen(false)}></div>
+                <div className="fixed w-full h-full bg-black opacity-50 z-[2000] md:hidden" onClick={() => setIsOpen(false)}/>
             }
 
             {/* menu (mobile) */}
@@ -55,22 +71,33 @@ export const PublicNavbar = () => {
                 
                 <div className="flex flex-col px-2 py-3 gap-2">
                     
-                    <button className="self-end bg-gray-100 inline-flex items-center justify-center p-2 rounded-md text-gray-500 hover:text-gray-700 hover:bg-gray-200 focus:outline-0"
-                        onClick={() => setIsOpen(false)} type="button">
-                            <X strokeWidth={2.5} size={20}></X>
-                    </button>
+                    <div className="flex md:hidden justify-end">
+                        <button className="bg-gray-200 items-center justify-center p-1.5 rounded-lg active:scale-80 transition duration-300 text-gray-500 focus:outline-none border-1 border-gray-300" 
+                            onClick={() => closeMobileMenu()} type="button">
+                                <X strokeWidth={2.5} size={20}></X>
+                        </button>
+                    </div>
 
-                    <span className="text-gray-500 border-t border-gray-300"></span>
+                    <span className="text-gray-500 border-b h-2 border-gray-300"></span>
 
-                    <Link href="/" onClick={() => setIsOpen(false)} className="text-gray-600 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-sm font-medium">
-                        Início
+                    <Link className="px-3 py-2 rounded-md" href="/">
+                        <span className="text-gray-600 text-sm transition duration-200 hover:bg-green-1 hover:text-green-900 font-medium">
+                            Início
+                        </span>
                     </Link>
-                    <Link href="/lines" onClick={() => setIsOpen(false)} className="text-gray-600 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-sm font-medium">
-                        Linhas
+                    
+                    <Link className="px-3 py-2 rounded-md"  href="/lines">
+                        <span className="text-gray-600  text-sm transition duration-200 hover:bg-green-1 hover:text-green-900 font-medium">
+                            Linhas
+                        </span>
                     </Link>
-                    <Link href="/dashboard" onClick={() => setIsOpen(false)} className="text-gray-600 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-sm font-medium">
-                        Acesso Restrito
+                    
+                    <Link className="px-3 py-2 rounded-md"  href="/login">
+                        <span className="text-gray-600 text-sm transition duration-200 hover:bg-green-1 hover:text-green-900 font-medium">
+                            Acesso Restrito
+                        </span>
                     </Link>
+
                 </div>
             </div>
         </nav>
